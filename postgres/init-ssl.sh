9a8ccd8f1f159039857356d77d74105ac6b0cca3
Andrés Olguín Dev <106123788+uwuolguin@users.noreply.github.com>
2025-11-29 00:27:07 -0300

    Update init-ssl.sh

--- postgres/init-ssl.sh
+++ postgres/init-ssl.sh
@@ -4,7 +4,6 @@ SSL_DIR="$PGDATA/ssl"
 mkdir -p "$SSL_DIR"
 chown postgres:postgres "$SSL_DIR"
 chmod 700 "$SSL_DIR"
-
 openssl req -new -x509 -days 3650 -nodes \
   -text -out "$SSL_DIR/server.crt" \
   -keyout "$SSL_DIR/server.key" \
@@ -16,3 +15,11 @@ chmod 600 "$SSL_DIR/server.key"
 echo "ssl = on" >> "$PGDATA/postgresql.conf"
 echo "ssl_cert_file = 'ssl/server.crt'" >> "$PGDATA/postgresql.conf"
 echo "ssl_key_file  = 'ssl/server.key'" >> "$PGDATA/postgresql.conf"
+
+echo "# FORCE TLS ONLY" >> "$PGDATA/pg_hba.conf"
+echo "hostssl all all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
+echo "hostssl all all ::0/0 md5"     >> "$PGDATA/pg_hba.conf"
+
+sed -i '/^host /d' "$PGDATA/pg_hba.conf"
+
+echo "TLS-only policy applied to pg_hba.conf"
