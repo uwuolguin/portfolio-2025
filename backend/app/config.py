64b5f9664d796864b47ae4b95e10adb357b6fd4e
uwuolguin <acos2014600836@gmail.com>
2026-02-10 23:44:44 -0300

    do 40

--- backend/app/config.py
+++ backend/app/config.py
@@ -160,7 +160,7 @@ class Settings(BaseSettings):
     verification_token_email_time: int = 30
     admin_email: str
     resend_api_key: str
-    email_from: str = "noreply@proveo.com"
+    email_from: str = "onboarding@resend.dev"
     api_base_url: str = "http://localhost"
 
 
