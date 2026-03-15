a66fc9b775722d0d5ac9b36928799f8726a0818b
uwuolguin <acos2014600836@gmail.com>
2026-03-14 23:43:27 -0300

    dopkp 26

--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Creating database role and database..."
+echo "Creating databases..."
 
 psql -v ON_ERROR_STOP=1 --username postgres <<-EOF
     DO
@@ -10,9 +10,14 @@ psql -v ON_ERROR_STOP=1 --username postgres <<-EOF
         IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'portfolio') THEN
             CREATE DATABASE portfolio OWNER "postgres";
         END IF;
+        IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'temporal') THEN
+            CREATE DATABASE temporal OWNER "postgres";
+        END IF;
+        IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'temporal_visibility') THEN
+            CREATE DATABASE temporal_visibility OWNER "postgres";
+        END IF;
     END
     \$\$
 EOF
 
-echo "database created (or already existed)"
-
+echo "Databases created (or already existed)"
